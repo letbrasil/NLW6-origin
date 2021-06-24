a1ab787c865dd41cbcdc1d06407843d9acea9a92
letbrasil <le.brasileiro@gmail.com>
2021-06-24 12:45:38 -0300

    add about and services sections

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
-const links = document.querySelectorAll('nav ul li a');
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
-    nav.classList.toggle('show');
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
   })
 }
 
 for (const link of links) {
-  link.addEventListener('click', function() {
-    nav.classList.remove('show');
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
+
+/* Mudar o header da página quando der scroll (sombreamento) */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
